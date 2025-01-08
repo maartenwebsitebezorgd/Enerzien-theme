c3d72df6d0b3ab9d6dbbe9e49006a6c59bd0617d
Maarten van Willigen <maarten@websitebezorgd.nl>
2025-01-08 16:43:23 +0100

    service def

--- app/public/wp-content/themes/qore/includes/partials/solutions/featured-solution-post.php
+++ app/public/wp-content/themes/qore/includes/partials/solutions/featured-solution-post.php
@@ -1,10 +1,11 @@
-<div class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 p-8 aspect-[3/4]">
+<div
+    class="relative isolate flex flex-col justify-end overflow-clip rounded-xl bg-gray-900 px-8 py-6 aspect-[3/4] shadow-lg hover:shadow-sm transition-shadow">
     <div class="service_media-wrap">
         <div class="absolute inset-0 -z-20">
             <?php echo wp_get_attachment_image($item['image']['ID'], 'post-thema', false, ['class' => 'object-cover object-center h-full w-full']); ?>
         </div>
-        <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+        <div class="absolute inset-0 -z-10 bg-gradient-to-t from-green-950 via-green-900/30"></div>
+        <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-green-950/10"></div>
     </div>
     <div class="service_body-wrap z-10">
         <div class="service_body-wrap flex flex-col">
@@ -14,7 +15,7 @@
                     <?php echo $item['title']; ?>
                 </a>
             </h3>
-            <p class="opacity-80 text-pretty text-white">
+            <p class="opacity-90 text-pretty text-white">
                 <?php echo $item['text']; ?>
             </p>
         </div>
